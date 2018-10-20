39ab9a74dac96ebe20a7842f6562df9da21a3332
Raghav <sragha45@gmail.com>
2018-10-20 19:11:29 +0530

    Less trash. Now texts all users about rating changes!

--- helper/codeforces.py
+++ helper/codeforces.py
@@ -87,22 +87,29 @@ def did_contest_really_end(contest_id):
     logging.info("Entering now!")
 
     url = "https://codeforces.com/api/contest.ratingChanges?contestId=" + str(contest_id)
-    with open("helper/cf_handles.json", "r+", encoding='utf-8') as f:
-        tracking_handles = json.load(f)["handles"]
-        tracking_handles = [x["handle"] for x in tracking_handles]
+
     try:
+        with open("helper/cf_handles.json", "r+", encoding='utf-8') as f:
+            tracking_handles = json.load(f)["handles"]
+            tracking_handles = [x["handle"] for x in tracking_handles]
+
         response = urllib.request.urlopen(url).read()
         response = json.loads(response)
         response = response["result"]
         result = []
-        handles = []
         for x in response:
             if x["handle"] in tracking_handles:
                 result.append(x)
-                handles.append(x["handle"])
-        return result, handles
+        return result
     except urllib.request.HTTPError:
         return False
+
+
+def get_user_data():
+    with open("helper/cf_handles.json", "r+", encoding='utf-8') as f:
+        data = json.load(f)
+        users = [x for x in data.keys() if x != "handles"]
+    return data, users
 
 
 def contest_finished(bot=None, job=None):
@@ -111,33 +118,27 @@ def contest_finished(bot=None, job=None):
     contest_id: str = context["id"]
     first_time = context["datetime"]
 
-    # Really trashy code. Fix this later.
-    contest_result, contest_result_handles = (None, None)
-    try:
-        contest_result, contest_result_handles = did_contest_really_end(contest_id)
-    except TypeError:
-        contest_result = False
+    contest_result = did_contest_really_end(contest_id)
 
-    if not contest_result:
-        print(first_time)
-        if datetime.now() > first_time + timedelta(minutes=1):
+    if contest_result is False:
+        # Change the delta later as per the need
+        if datetime.now() < first_time + timedelta(minutes=1):
             job_queue.run_once(contest_finished,
                                when=datetime.now() + timedelta(seconds=30),
                                context=context)
         else:
-            with open("helper/cf_handles.json", "r+", encoding='utf-8') as f:
-                data = json.load(f)
-                users = [x for x in data.keys() if x != "handles"]
-                for user in users:
-                    bot.send_message(chat_id=user,
-                                     text="Cannot retrieve details for contest: " + str(contest_id))
+            data, users = get_user_data()
+            for user in users:
+                bot.send_message(chat_id=user,
+                                 text="Cannot retrieve details for contest: " + str(contest_id))
     else:
-        with open("helper/cf_handles.json", "r+", encoding='utf-8') as f:
-            data = json.load(f)
-            users = [x for x in data.keys() if x != "handles"]
-
-        for user in users:
-            for handle in data[user]:
-                if handle in contest_result_handles:
+        data, users = get_user_data()
+        for x in contest_result:
+            for user in users:
+                logging.info(json.dumps(x, indent=4))
+                msg = "Profile: " + x["handle"] + "\n" \
+                      + "Rating change = " + str(x["oldRating"]) + " \u2192 " + str(x["newRating"]) + " ( " \
+                      + "{:+}".format(x["newRating"] - x["oldRating"]) + ")"
+                if x["handle"] in data[user]:
                     bot.send_message(chat_id=user,
-                                     text=handle)
+                                     text=msg)
